a048f6719368b05c10eb89bcf9449483d7b1b148
Xlerion Dev <dev@xlerion.com>
2026-03-18 20:12:17 -0500

    chore: support Railway DATABASE_URL -> Npgsql connection string conversion

--- Program.cs
+++ Program.cs
@@ -22,8 +22,24 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+string connectionString;
+
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    // Convierte formato URI de Railway a formato Npgsql
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')}" +
+                       $";Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+}
+else
+{
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
 
