8cb38848eaf606eb7d97732b3a21473da08ffca3
Xlerion Dev <dev@xlerion.com>
2026-03-18 20:38:21 -0500

    chore: enable Swagger in all environments

--- Program.cs
+++ Program.cs
@@ -74,15 +74,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Xlerion Leaderboard API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Xlerion Leaderboard API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 // Global JSON exception handler
 app.UseExceptionHandler(appBuilder =>
